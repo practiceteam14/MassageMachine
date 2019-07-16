f03112341917363ea2fe1e089fae4be334dd66a9
JangeSeongHo <qq9725@ajou.ac.kr>
2019-07-16 18:22:34 +0900

    Add to massage-machine spring boot project

---
+++ src/main/java/org/ajou/realcoding/massagemachine/massagemachine/MassageMachineApplication.java
@@ -0,0 +1,13 @@
+package org.ajou.realcoding.massagemachine.massagemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassageMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassageMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ajou/realcoding/massagemachine/massagemachine/MassageMachineApplicationTests.java
@@ -0,0 +1,16 @@
+package org.ajou.realcoding.massagemachine.massagemachine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MassageMachineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
