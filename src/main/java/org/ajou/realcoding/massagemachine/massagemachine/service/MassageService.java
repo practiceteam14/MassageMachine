d279c304693b3dec76f6d1d6926e974d28bcbe00
JangeSeongHo <qq9725@ajou.ac.kr>
2019-07-19 20:24:56 +0900

    Create method select massage mode by mode-name

--- src/main/java/org/ajou/realcoding/massagemachine/massagemachine/service/MassageService.java
+++ src/main/java/org/ajou/realcoding/massagemachine/massagemachine/service/MassageService.java
@@ -1,11 +1,20 @@
 package org.ajou.realcoding.massagemachine.massagemachine.service;
 
 import org.ajou.realcoding.massagemachine.massagemachine.repository.MassageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class MassageService {
+    @Autowired
     private MassageRepository massageRepository;
 
     public void addMassageModeByModeName(String wantModeName) {
-        massageRepository.insertModeRandomlyBodyParyAndPower(wantModeName);
+        massageRepository.insertModeRandomlyBodyPartAndPower(wantModeName);
+    }
+
+    public void selectMassageModeByModeName(String wantModeName) {
+        for (int i = 0; i<massageRepository.findAll().size(); i++) {
+            log.info("마사지 모드 : {}", massageRepository.findAll().get(i));
+        }
+        return massageRepository.findMassageModeByModeName(wantModeName);
     }
 }
