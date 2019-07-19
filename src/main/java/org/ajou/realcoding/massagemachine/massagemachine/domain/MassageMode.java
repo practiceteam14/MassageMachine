771d240c046350ec8a07118729a99426aba0566d
JangeSeongHo <qq9725@ajou.ac.kr>
2019-07-19 20:37:50 +0900

    Add AllArgsConstructor to MassageMode

--- src/main/java/org/ajou/realcoding/massagemachine/massagemachine/domain/MassageMode.java
+++ src/main/java/org/ajou/realcoding/massagemachine/massagemachine/domain/MassageMode.java
@@ -1,20 +1,14 @@
 package org.ajou.realcoding.massagemachine.massagemachine.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+@AllArgsConstructor
 @Data
 public class MassageMode {
     private String wantMode;
     private String bodyPart;
     private String power;
     private int time;
-
-    @Builder
-    public MassageMode(String wantModeName, String bodyPart, String power, int time) {
-        this.wantMode = wantModeName;
-        this.bodyPart = bodyPart;
-        this.power = power;
-        this.time = time;
-    }
 }
