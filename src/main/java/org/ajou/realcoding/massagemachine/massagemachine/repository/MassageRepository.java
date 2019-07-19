14ce88e6cdf586e2c0369037cee3f60722a2b071
JangeSeongHo <qq9725@ajou.ac.kr>
2019-07-19 20:21:40 +0900

    Create method that insert domain to repository

--- src/main/java/org/ajou/realcoding/massagemachine/massagemachine/repository/MassageRepository.java
+++ src/main/java/org/ajou/realcoding/massagemachine/massagemachine/repository/MassageRepository.java
@@ -1,4 +1,24 @@
 package org.ajou.realcoding.massagemachine.massagemachine.repository;
 
+import org.ajou.realcoding.massagemachine.massagemachine.domain.MassageMode;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Random;
+
 public class MassageRepository {
+    @Autowired
+    List<MassageMode> modes;
+
+    @Autowired
+    Random random;
+
+    public void insertModeRandomlyBodyPartAndPower(String wantModeName) {
+        String bodyPart[] = {"목", "어깨", "팔", "다리", "발"};
+        String power[] = {"강", "중", "약"};
+        int time = random.nextInt(10);
+        MassageMode massageMode = new MassageMode(wantModeName, bodyPart[random.nextInt(bodyPart.length-1)],
+                power[random.nextInt(power.length-1)], time);
+        modes.add(massageMode);
+    }
 }
